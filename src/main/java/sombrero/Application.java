7b3d2ab8f1c4b9fa1e737f5c8f798da9a4add5f3
rey <glengleann@gmail.com>
2020-04-01 16:21:53 +0900

    .

--- src/main/java/sombrero/Application.java
+++ src/main/java/sombrero/Application.java
@@ -2,9 +2,6 @@ package sombrero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Application {
