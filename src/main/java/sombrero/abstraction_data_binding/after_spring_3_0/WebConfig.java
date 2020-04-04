b827173ca60464ffcf4522c6c2acd99d3475ccac
rey <glengleann@gmail.com>
2020-04-04 20:19:18 +0900

    .

--- src/main/java/sombrero/abstraction_data_binding/after_spring_3_0/WebConfig.java
+++ src/main/java/sombrero/abstraction_data_binding/after_spring_3_0/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 이곳에서 추가한 Converter가 모든 컨트롤러에 동작.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
